8ee3be4e4fc43eda2135621002545ccdd01e1708
Muhammad Bilal <bscs.f8.m16.a@gmail.com>
2023-12-25 03:36:47 +0500

    move images to phototesting page and make the root page simple with two links

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,14 @@ export default function Home() {
   return (
     <main className="container mx-auto">
       <h1 className="text-center text-4xl font-bold m-10">NextGram</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 auto-rows-max	 gap-6 m-10">
-        {photos.map(({ id, imageSrc }: any) => (
-          <Link key={id} href={`/phototesting/photos/${id}`}>
-            <Image
-              alt=""
-              src={imageSrc}
-              height={500}
-              width={500}
-              className="w-full object-cover aspect-square"
-            />
-          </Link>
-        ))}
-      </div>
+      <div>Go to Image</div>
+      <Link className="underline" href={"/phototesting"}>
+        Photo testing Page
+      </Link>
+      <br />
+      <Link className="underline" href={"/phototesting/photos/2"}>
+        Direct To Image 2
+      </Link>
     </main>
   );
 }
--- app/phototesting/page.tsx
+++ app/phototesting/page.tsx
@@ -1,7 +1,29 @@
+import photos from "@/photos";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const PhotoTestingPage = () => {
-  return <div>PhotoTestingPage</div>;
+  return (
+    <main className="container mx-auto">
+      <h1 className="text-center text-4xl font-bold m-10">
+        Photo Testing Page
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 auto-rows-max	 gap-6 m-10">
+        {photos.map(({ id, imageSrc }: any) => (
+          <Link key={id} href={`/phototesting/photos/${id}`}>
+            <Image
+              alt=""
+              src={imageSrc}
+              height={500}
+              width={500}
+              className="w-full object-cover aspect-square"
+            />
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default PhotoTestingPage;
